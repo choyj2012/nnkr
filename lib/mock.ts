d7e917948ba61dbe9c86438068c692c358c7e3b1
choyj2012 <choyj2012@gmail.com>
2024-04-04 20:00:08 +0900

    add mock data

--- lib/mock.ts
+++ lib/mock.ts
@@ -33,5 +33,38 @@ export const Q: Question[] = [
     dora: 'Z1',
     description: 'question1',
     answer: '?',
+  },
+
+  {
+    id: 104,
+    tehai: ['S1', 'S2', 'S3', 'S4', 'S5', 'S51', 'S6', 'S7', 'S8', 'S9', 'Z1', 'Z2', 'Z3'],
+    tsumo: 'P2',
+    kyokumen: '동 1',
+    junme: 4,
+    dora: 'Z1',
+    description: 'question1',
+    answer: '?',
+  },
+
+  {
+    id: 105,
+    tehai: ['M1', 'M2', 'M3', 'M4', 'M5', 'M51', 'M6', 'M7', 'M8', 'M9', 'Z2', 'Z3', 'Z4'],
+    tsumo: 'P2',
+    kyokumen: '동 1',
+    junme: 4,
+    dora: 'Z1',
+    description: 'question1',
+    answer: '?',
+  },
+
+  {
+    id: 106,
+    tehai: ['P1', 'P2', 'P3', 'P4', 'P5', 'P51', 'P6', 'P7', 'P8', 'P9', 'Z5', 'Z6', 'Z7'],
+    tsumo: 'P2',
+    kyokumen: '동 1',
+    junme: 4,
+    dora: 'Z1',
+    description: 'question1',
+    answer: '?',
   }
 ]
