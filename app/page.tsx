63603158cb1632a3ed043b6300ddc1a0e4cf53b6
choyj2012 <choyj2012@gmail.com>
2024-05-09 20:37:40 +0900

    revalidate test

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,26 @@
 import Card from "@/components/Card/card";
 import { getAllQuestions } from "@/lib/queries";
+import { Question } from "@/lib/types";
 import Link from "next/link";
 
 export default async function Home() {
   
-  const Q = await getAllQuestions();
+  const Q = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/questions`, {
+    next: {
+      revalidate: 10,
+    }
+  }).then(res => {
+    return res.json()
+  }).then(res => {
+    console.log(res);
+    return res;
+  })
+  //const Q = await getAllQuestions();
 
   return (
     <main>
       <div className="flex flex-col w-full">
-        {Q?.map((quest) => {
+        {Q?.map((quest: Question) => {
           return (
             <Link key={quest.id} href={`/questions/${quest.id}`}>
               <div className="*:hover:bg-green-50 *:hover:transition-colors cursor-pointer">
