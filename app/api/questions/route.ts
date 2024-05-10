21f0cde47d70f166abbc4dec040e414ccc565394
choyj2012 <choyj2012@gmail.com>
2024-05-10 15:31:22 +0900

    redirect 수정

--- app/api/questions/route.ts
+++ app/api/questions/route.ts
@@ -3,11 +3,13 @@ import { addQuestion, getAllQuestions } from "@/lib/queries";
 import { revalidatePath } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 import { redirect } from "next/navigation";
+import { Question } from "@/lib/types";
 
 export async function POST(req: Request) {
-  const res = await addQuestion(await req.json());
+  const q: Question = await req.json();
+  const res = await addQuestion(q);
   revalidatePath('/')
-  redirect('/');
+  redirect(`/questions/${q.id}`);
   return NextResponse.json(res);
 }
 
