eca80aa6086533e1a7b6a6cb51ec75d82002d746
choyj2012 <choyj2012@gmail.com>
2024-09-09 16:37:18 +0900

    suspense test

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import CardList from "@/components/CardList/cardList";
+import Skeleton from "@/components/Skeleton/skeleton";
+import { Suspense } from "react";
 
 export default async function Home() {
 
   return (
     <main>
       <div className="flex flex-col w-full gap-4">
-        <CardList />
+        <Suspense fallback={<Skeleton/>}>
+          <CardList />
+        </Suspense>
       </div>
     </main>
   );
